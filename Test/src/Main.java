a917cfb2c6fec4e145ea8e69bc1b14b673d05473
stagiaire <stagiaire@VM-Ubuntu>
2019-06-14 11:44:02 +0200

    Save, commit projet RLBanque

--- Test/src/Main.java
+++ Test/src/Main.java
@@ -1,35 +1,57 @@
-import java.util.Scanner;
-
+//Packages à importer afin d'utiliser l'objet File
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+ 
 public class Main {
-
-	public static void main(String[] args) {
-		
-		//Définition d'un tableau de villes null
-		Ville[] tableau = new Ville[6];
-		        
-		//Définition d'un tableau de noms de Villes et un autre de nombres d'habitants
-		String[] tab = {"Marseille", "lille", "caen", "lyon", "paris", "nantes"};
-		int[] tab2 = {123456, 78456, 654987, 75832165, 1594, 213};
-		         
-		//Les trois premiers éléments du tableau seront des Villes
-		//et le reste des capitales
-		for(int i = 0; i < 6; i++){
-		  if (i <3){
-		    Ville V = new Ville(tab[i], tab2[i], "france");
-		    tableau[i] = V;
-		  }
-		         
-		  else{
-		    Capitale C = new Capitale(tab[i], tab2[i], "france", "la tour Eiffel");
-		    tableau[i] = C;
-		  }
-		}
-		                 
-		//Il ne nous reste plus qu'à décrire tout notre tableau !
-		for(Object obj : tableau){
-		  System.out.println(obj+"\n");
-		}
-	}
-
-
+  public static void main(String[] args) {
+    //Nous déclarons nos objets en dehors du bloc try/catch
+    FileInputStream fis;
+    FileOutputStream fos;
+    BufferedInputStream bis;
+    BufferedOutputStream bos; 
+        
+    try {
+      fis = new FileInputStream(new File("test.txt"));
+      fos = new FileOutputStream(new File("test2.txt"));
+      bis = new BufferedInputStream(new FileInputStream(new File("test.txt")));
+      bos = new BufferedOutputStream(new FileOutputStream(new File("test3.txt")));
+      byte[] buf = new byte[8];
+ 
+      //On récupère le temps du système
+      long startTime = System.currentTimeMillis();
+                
+      while(fis.read(buf) != -1){
+        fos.write(buf);
+      }
+      //On affiche le temps d'exécution
+      System.out.println("Temps de lecture + écriture avec FileInputStream et FileOutputStream : " + (System.currentTimeMillis() - startTime));
+                
+      //On réinitialise                
+      startTime = System.currentTimeMillis();
+ 
+      while(bis.read(buf) != -1){
+        bos.write(buf);
+      }
+      //On réaffiche
+      System.out.println("Temps de lecture + écriture avec BufferedInputStream et BufferedOutputStream : " + (System.currentTimeMillis() - startTime));
+                
+      //On ferme nos flux de données
+      fis.close();
+      bis.close();
+      fos.close();
+      bos.close();
+                
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }     	
+  }
 }
+  
+  
